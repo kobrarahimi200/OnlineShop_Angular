2f2db69a838b4fa89cddf11e18cdba41e1f38392
kobrarahimi200 <k.rahimi.npa@gmail.com>
2022-05-02 12:03:54 +0200

    create new directive named highlight

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,13 +1,23 @@
-import { Directive, ElementRef, Renderer2 ,OnInit} from '@angular/core';
+import { Directive, ElementRef, Renderer2 ,OnInit, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit{
 
-  constructor(private elErf: ElementRef, private rendered: Renderer2) { }
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  constructor(private elErf: ElementRef, private rendered: Renderer2) { } 
 
   ngOnInit(){
-    this.rendered.setStyle(this.elErf.nativeElement, 'background-color', 'red');
+    // this.rendered.setStyle(this.elErf.nativeElement, 'background-color', 'red');
+  }
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    // this.rendered.setStyle(this.elErf.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue'
+  } 
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    // this.rendered.setStyle(this.elErf.nativeElement, 'background-color', 'red');
+    this.backgroundColor = 'red'
+    
   }
 }
